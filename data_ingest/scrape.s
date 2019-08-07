2ec3169b1a03d52bc48134b16054b3bff8e21841
Jane Liu <janeikeliu@gmail.com>
2019-08-07 09:40:27 -0400

    Updated the scrape.s batch file if running on Prince server.

--- data_ingest/scrape.s
+++ data_ingest/scrape.s
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 ##SBATCH --nodes=1
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=6:00:00
-#SBATCH --mem=50GB
+#SBATCH --mem=100GB
 #SBATCH --job-name=BDAD_Project
 #SBATCH --mail-type=END
 ##SBATCH --mail-user=jl860@nyu.edu
@@ -16,7 +16,8 @@ module load python3/intel/3.6.3
 
 cd /scratch/jl860/bdad/fp
 source env/bin/activate
-python scraper_nyc.py
+cd /scratch/jl860/bdad/fp/data_ingest
+python scraper.py
 
 
 
